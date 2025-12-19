5c6b791e6aa24a379d8e2d5395006fceaef1c5fa
Adrien DEU <adrien.deu@epita.fr>
2025-12-19 13:42:32 +0100

    feat(dust-chart): Cleanup dust-chart component files and update spec

---
+++ frontend/src/app/components/deck-builder/dust-chart/dust-chart.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { DustChartComponent } from './dust-chart.component';
+
+describe('DustChartComponent', () => {
+  let component: DustChartComponent;
+  let fixture: ComponentFixture<DustChart>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [DustChartComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(DustChartComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
